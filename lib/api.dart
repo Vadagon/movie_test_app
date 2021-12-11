4d871f6f079fff3c3fc0949c3a4a29aacac80f69
Vladyslav <verbalike@gmail.com>
2021-12-11 18:32:58 +0200

    most popular movies

--- lib/api.dart
+++ lib/api.dart
@@ -5,7 +5,7 @@ import 'package:movie_test_app/model.dart';
 
 Future<List> fetchMovies() async {
   final response = await http.get(Uri.parse(
-      'https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key=33f3cf629308b46ddfc62c05e50db435'));
+      'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=33f3cf629308b46ddfc62c05e50db435'));
 
   if (response.statusCode == 200) {
     Map res = jsonDecode(response.body);
